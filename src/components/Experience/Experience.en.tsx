0057c1c86ddaf2c80de935a56ab1a96a49ca9253
Sivert Espeland Husebø <siverteh@student.uia.no>
2024-01-29 13:58:14 +0100

    Added new CV and updated experience section

--- src/components/Experience/Experience.en.tsx
+++ src/components/Experience/Experience.en.tsx
@@ -14,8 +14,25 @@ const ExperienceEn = () => {
                         <div className="timeline-year">06/2024 - 08/2024</div>
                         <div className="timeline-company">NorKart</div>
                         <p className="timeline-description">
-                            I will be working as a summer intern at NorKart, engaging in practical projects and gaining hands-on experience in the field.
+                            I will be working as a summer intern at NorKart, engaging in practical projects and gaining
+                            hands-on experience in the field.
                         </p>
+                    </div>
+                </div>
+                <div className="timeline-item">
+                    <div className="timeline-content">
+                        <div className="timeline-circle"></div>
+                        <h3>Student Assistant</h3>
+                        <div className="timeline-year">01/2024 - Current</div>
+                        <div className="timeline-company">University of Agder</div>
+                        <ul className="timeline-description">
+                            <li>Assisted students during lab hours and helped them understand the course material.</li>
+                            <li>Provided guidance and support for students on academic-related questions and
+                                assignments.
+                            </li>
+                            <li>Evaluated submissions and provided feedback to enhance students' learning.</li>
+                            <li>Collaborated with faculty to improve teaching methods and course content.</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-item">
@@ -24,20 +41,45 @@ const ExperienceEn = () => {
                         <h3>Security Guard</h3>
                         <div className="timeline-year">08/2021 - 08/2023</div>
                         <div className="timeline-company">Personal Service and Security (PSS)</div>
-                        <p className="timeline-description">
-                            As a Security Guard at PSS, I was responsible for ensuring the safety and security of premises, managing access control, and responding to emergencies.
-                        </p>
+                        <ul className="timeline-description">
+                            <li>Conducted regular security rounds and monitored premises to ensure safety and prevent
+                                unauthorized access.
+                            </li>
+                            <li>Managed access control and identified visitors to maintain security protocols at entry
+                                points.
+                            </li>
+                            <li>Contributed to the development and implementation of enhanced security measures and
+                                procedures.
+                            </li>
+                            <li>Maintained detailed records and reported security incidents and observations to
+                                superiors.
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-item">
                     <div className="timeline-content">
                         <div className="timeline-circle"></div>
-                        <h3>Military Service</h3>
+                        <h3>Private</h3>
                         <div className="timeline-year">08/2020 - 08/2021</div>
                         <div className="timeline-company">Norwegian Armed Forces, Setermoen leir</div>
-                        <p className="timeline-description">
-                            My military service at Setermoen leir involved rigorous training and skill development in various military disciplines. This experience honed my abilities in teamwork, discipline, and leadership, providing me with a strong foundation for professional challenges.
-                        </p>
+                        <ul className="timeline-description">
+                            <li>Completed basic military training, including physical fitness, weapon handling, and
+                                tactical training.
+                            </li>
+                            <li>Actively participated in field exercises and maneuvers, developing teamwork and
+                                leadership skills under pressure.
+                            </li>
+                            <li>Performed regular maintenance of military equipment and vehicles to ensure operational
+                                readiness.
+                            </li>
+                            <li>Participated in courses and training in first aid, survival techniques, and crisis
+                                management.
+                            </li>
+                            <li>Effectively collaborated with the unit to execute missions and participate in organized
+                                drills and exercises.
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- src/components/Experience/Experience.no.tsx
+++ src/components/Experience/Experience.no.tsx
@@ -14,8 +14,23 @@ const ExperienceNo = () => {
                         <div className="timeline-year">06/2024 - 08/2024</div>
                         <div className="timeline-company">NorKart</div>
                         <p className="timeline-description">
-                            Jeg skal jobbe som sommerintern hos NorKart, hvor jeg vil delta i praktiske prosjekter og få hands-on erfaring i feltet.
+                            Jeg skal jobbe som sommerintern hos NorKart, hvor jeg vil delta i praktiske prosjekter og få
+                            hands-on erfaring i feltet.
                         </p>
+                    </div>
+                </div>
+                <div className="timeline-item">
+                    <div className="timeline-content">
+                        <div className="timeline-circle"></div>
+                        <h3>Studentassistent</h3>
+                        <div className="timeline-year">01/2024 - Nåværende</div>
+                        <div className="timeline-company">Universitetet i Agder</div>
+                        <ul className="timeline-description">
+                            <li>Hjelper studenter under labtimer og sikrer forståelse av fagstoff.</li>
+                            <li>Gir veiledning og støtte til studenter på fagrelaterte spørsmål og oppgaver.</li>
+                            <li>Retter innleveringer og gir tilbakemeldinger for å forbedre studentenes læring.</li>
+                            <li>Samarbeider med faglærere for å forbedre undervisningsmetoder og faginnhold.</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-item">
@@ -24,20 +39,41 @@ const ExperienceNo = () => {
                         <h3>Vekter</h3>
                         <div className="timeline-year">08/2021 - 08/2023</div>
                         <div className="timeline-company">Personal Service og Sikkerhet (PSS)</div>
-                        <p className="timeline-description">
-                            Som vekter hos PSS var jeg ansvarlig for å sikre tryggheten og sikkerheten på ulike steder, håndtere adgangskontroll, og respondere på nødsituasjoner.
-                        </p>
+                        <ul className="timeline-description">
+                            <li>Utførte regelmessige sikkerhetsrunder og overvåket eiendom for å sikre trygghet og
+                                forebygge uautorisert adgang.
+                            </li>
+                            <li>Håndterte tilgangskontroll og identifiserte besøkende for å opprettholde
+                                sikkerhetsprotokoller ved inngangspunkter.
+                            </li>
+                            <li>Bidro til utvikling og implementering av forbedrede sikkerhetstiltak og prosedyrer.</li>
+                            <li>Opprettholdt detaljerte loggføringer og rapporterte sikkerhetshendelser og observasjoner
+                                til overordnede.
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <div className="timeline-item">
                     <div className="timeline-content">
                         <div className="timeline-circle"></div>
-                        <h3>Militærtjeneste</h3>
+                        <h3>Menig</h3>
                         <div className="timeline-year">08/2020 - 08/2021</div>
                         <div className="timeline-company">Norske Forsvaret, Setermoen leir</div>
-                        <p className="timeline-description">
-                            Min militærtjeneste ved Setermoen leir involverte intensiv trening og utvikling av ferdigheter i ulike militære disipliner. Denne erfaringen styrket mine evner innen teamarbeid, disiplin, og lederskap, noe som har gitt meg et solid fundament for profesjonelle utfordringer.
-                        </p>
+                        <ul className="timeline-description">
+                            <li>Gjennomførte grunnleggende militærtrening, inkludert fysisk trening, våpenhåndtering, og
+                                taktisk trening.
+                            </li>
+                            <li>Deltok aktivt i feltøvelser og manøvrer, og utviklet ferdigheter i teamarbeid og ledelse
+                                under press.
+                            </li>
+                            <li>Utførte regelmessig vedlikehold av militært utstyr og kjøretøy for å sikre operativ
+                                beredskap.
+                            </li>
+                            <li>Deltok i kurs og opplæring i førstehjelp, overlevelsesteknikker og krisehåndtering.</li>
+                            <li>Samarbeidet effektivt med enheten for å utføre oppdrag og deltakelse i organiserte
+                                treninger og øvelser.
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
